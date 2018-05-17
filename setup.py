710379d02da549b602a321a69fa0546c33edbe28
Peyman Mortazavi <peyman.mortazavi@infoscoutinc.com>
2018-05-17 09:11:07 -0400

    Update setup files

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ with open('VERSION', 'r') as f:
     version = f.read().strip()
 
 setup(
+    author='Peyman Mortazavi',
+    author_email='pey.mortazavi@gmail.com',
     name='garlicconfig',
     packages=find_packages(),
     include_package_data=True,
     description='InfoScout GarlicConfig',
-    url='http://github.com/infoscout/garlicconfig',
+    url='https://github.com/infoscout/garlicconfig',
+    download_url='https://github.com/infoscout/garlicconfig/archive/{version}.tar.gz'.format(version=version),
     version=version,
+    keywords=['configs', 'settings', '']
 )
