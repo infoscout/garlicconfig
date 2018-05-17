05abb0060fd95b460b1d5eb181db1087b6731da7
Peyman Mortazavi <peyman.mortazavi@infoscoutinc.com>
2018-05-17 09:42:50 -0400

    Provide long description

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import find_packages, setup
 
-with open('VERSION', 'r') as f:
-    version = f.read().strip()
+with open('VERSION', 'r') as reader:
+    version = reader.read().strip()
+
+
+def get_long_description():
+    with open('README.md', 'r') as reader:
+        return reader.read()
+
 
 setup(
     author='Peyman Mortazavi',
@@ -10,8 +16,13 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     description='InfoScout GarlicConfig',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown; charset=UTF-8',
     url='https://github.com/infoscout/garlicconfig',
     download_url='https://github.com/infoscout/garlicconfig/archive/{version}.tar.gz'.format(version=version),
     version=version,
-    keywords=['configs', 'settings', '']
+    keywords=['configs', 'settings', ''],
+    package_data={
+        'garlicconfig': ['VERSION', 'README.md']
+    }
 )
