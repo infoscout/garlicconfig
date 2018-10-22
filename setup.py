de356f314758d4882148762634549d085dc2c6ec
Peyman Mortazavi <peyman.mortazavi@infoscoutinc.com>
2018-10-21 20:31:08 -0400

    Added static linking and mac support

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ common_params = dict(
     language='c++',
     include_dirs=['cget/include'],
     library_dirs=['cget/lib', 'cget/lib64'],
-    libraries=['GarlicConfig'],
+    libraries=['GarlicConfig', 'boost_filesystem', 'boost_system'],
     extra_compile_args=['-std=c++11']
 )
 
