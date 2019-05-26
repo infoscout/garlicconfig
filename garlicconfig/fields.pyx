39df8af8992d915ed544dc3bae18cda35f23ab03
Peyman <pey.mortazavi@gmail.com>
2019-05-26 18:30:48 -0400

    use cpdef for validate_model_fields

--- garlicconfig/fields.pyx
+++ garlicconfig/fields.pyx
@@ -5,7 +5,7 @@ import six
 from six.moves import map
 
 
-def validate_model_fields(model):
+cpdef validate_model_fields(model):
     cdef ConfigField field
     cdef str key
     for key, field in six.iteritems(model.__meta__.fields):
