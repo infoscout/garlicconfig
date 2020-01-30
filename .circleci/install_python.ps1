27b8c4dd0d96420f7afbd37e85b42a2859f0ad88
Peyman <pey.mortazavi@gmail.com>
2020-01-30 00:35:01 -0500

    updated env refs

--- .circleci/install_python.ps1
+++ .circleci/install_python.ps1
@@ -1,7 +1,7 @@
 cd ~
-nuget install $env:PYTHON_NAME -Version $env:PYTHON_VERSION -ExcludeVersion -OutputDirectory .
-$env:PYTHON_NAME\tools\python.exe -m pip install virtualenv
-$env:PYTHON_NAME\tools\python.exe -m virtualenv garlic
+nuget install $PYTHON_NAME -Version $PYTHON_VERSION -ExcludeVersion -OutputDirectory .
+$PYTHON_NAME\tools\python.exe -m pip install virtualenv
+$PYTHON_NAME\tools\python.exe -m virtualenv garlic
 garlic\Scripts\activate.ps1
 cd project
 pip install -r requirements.txt
