31426770235c49c7414553b823eab96ca8aa7873
Peyman <pey.mortazavi@gmail.com>
2020-01-30 01:34:32 -0500

    update native install script

--- .circleci/install_libs.ps1
+++ .circleci/install_libs.ps1
@@ -1,3 +1,7 @@
 ~\garlic\Scripts\activate.ps1
 cget init --std=c++ --static
-cget install -DCMAKE_GENERATOR_PLATFORM=$env:ARCH --file=native_requirements.txt
+if ($env:ARCH) {
+	cget install -DCMAKE_GENERATOR_PLATFORM=x64 --file=native_requirements.txt
+} else {
+	cget install --file=native_requirements.txt
+}
