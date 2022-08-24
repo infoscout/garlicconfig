c021b259943a5f591b76e3de55beeacc40938921
Ryan Krabath <ryan.krabath@numerator.com>
2022-08-24 17:06:18 -0500

    Don't build as py3, it doesn't work

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     version=version,
     install_requires=['six'],
     keywords=['configs', 'settings'],
-    ext_modules=cythonize(ext_modules, language_level="3"),
+    ext_modules=cythonize(ext_modules),
     zip_safe=False
 )
