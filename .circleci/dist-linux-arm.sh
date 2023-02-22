761d7055d4a993089d87a2fc2a4bc806c373a9ce
Aaron Eikenberry <email@aaroneikenberry.com>
2023-02-22 15:19:30 -0600

    add linux dist

---
+++ .circleci/dist-linux-arm.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+CURRENT_DIR=$(pwd)
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CURRENT_DIR/cget/lib:$CURRENT_DIR/cget/lib64
+
+rm -fr build
+rm -fr dist
+
+
+repair_dist () {
+    for i in $(ls dist);
+    do
+        auditwheel repair --plat manylinux_2_24_aarch64 dist/$i
+    done;
+    rm -fr build
+    rm -fr dist
+}
+
+
+python3 setup.py bdist_wheel --plat-name=linux-aarch64
+repair_dist
